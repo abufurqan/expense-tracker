bacd556b65d8b84f46b750343408af8eb3e92fd0
Fiaz Ahmed <fayyazahmada@gmail.com>
2020-06-13 06:45:17 +0300

    changes at app.css and app.js

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,41 @@
+import React, {useState, useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
+
+export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+
+    addTransaction(newTransaction);
+  }
+
+  return (
+    <>
+      <h3>Add New Transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Description of Expense</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter Description of Expense..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount"
+            >Amount <br />
+            (Enter Negative Amount for Expense and Positive Amount for Income)</label
+          >
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  )
+}
